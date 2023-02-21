0b1e30c8f30c692a305ac2d707c827d4e33eca83
Karol Drożak <karoldro@gmail.com>
2023-02-21 13:22:04 +0100

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+// Based on with modification: https://www.youtube.com/watch?v=9WOUDlTS-rg
+
+use std::fs::File;
+use std::net::Ipv4Addr;
+
+#[derive(Debug)]
+enum CustomError {
+    IOErr(std::io::Error),
+    AddrErr(std::net::AddrParseError),
+    StdErr(Box<dyn std::error::Error>),
+}
+
+impl std::fmt::Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "CustomError {:?}", self)
+    }
+}
+
+impl From<std::io::Error> for CustomError {
+    fn from(error: std::io::Error) -> Self {
+        CustomError::IOErr(error)
+    }
+}
+
+impl From<std::net::AddrParseError> for CustomError {
+    fn from(error: std::net::AddrParseError) -> Self {
+        CustomError::AddrErr(error)
+    }
+}
+
+impl From<Box<dyn std::error::Error>> for CustomError {
+    fn from(error: Box<dyn std::error::Error>) -> Self {
+        CustomError::StdErr(error)
+    }
+}
+
+fn main() -> Result<(), CustomError> {
+    println!("Error handling example!");
+    custom_error_handling()?;
+    std_error_handling()?;
+    Ok(())
+}
+
+fn custom_error_handling() -> Result<(), CustomError> {
+    let _net = File::open("some-non-existant-file.txt")?;
+    let _localhost: Ipv4Addr = "::1".parse()?;
+    std_error_handling()?;
+    Ok(())
+}
+
+fn std_error_handling() -> Result<(), Box<dyn std::error::Error>> {
+    let _net = File::open("some-non-existant-file.txt")?;
+    let _localhost: Ipv4Addr = "::1".parse()?;
+    Ok(())
+}
